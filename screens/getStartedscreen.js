d3c7e9d00a15cd23640ef349b6f5974e70ab2d9f
shubham <worknshubh@outlook.com>
2025-07-05 01:07:10 +0530

    modifications in getStarted

--- screens/getStartedscreen.js
+++ screens/getStartedscreen.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import primary_color from '../components/colors';
-
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
 const Getstartedscreen = () => {
   return (
     <SafeAreaView style={styles.mainScreen}>
-      <View></View>
+      <View>
+        <Text style={styles.punchline}>Real Time Rates</Text>
+      </View>
     </SafeAreaView>
   );
 };
@@ -13,6 +18,13 @@ const styles = StyleSheet.create({
   mainScreen: {
     backgroundColor: primary_color,
     flex: 1,
+    padding: wp('10%'),
+  },
+  punchline: {
+    color: 'white',
+    fontFamily: 'Kanit-Regular',
+    fontSize: 30,
+    textAlign: 'center',
   },
 });
 export default Getstartedscreen;
