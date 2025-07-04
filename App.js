0e76cfbe57643d38fb4e6330c44566b57ccf4cb5
shubham <worknshubh@outlook.com>
2025-07-05 00:04:11 +0530

    create app screen

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+const Stack = createNativeStackNavigator();
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator></Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+export default App;
